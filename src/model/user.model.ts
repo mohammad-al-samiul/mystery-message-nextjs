9e2c1873b2468e9c409bb313b3e9b520757db746
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-03-21 14:43:53 +0600

    user schema and its type added

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -16,3 +16,53 @@ const messageSchema = new Schema<IMessage>({
     default: Date.now,
   },
 });
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: IMessage[];
+}
+
+const userSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: [true, "Username is required!"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Please use valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify Code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify Code Expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [messageSchema],
+});
